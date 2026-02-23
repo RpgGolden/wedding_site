0f003600dc58c8baa14db70ff946532cf4937755
RpgGold <4erepaxa45@mail.ru>
2026-02-23 18:39:02 +0300

    add friends

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-window.addEventListener("load", () => {
-  setTimeout(() => {
-    document.body.classList.add("open");
-  }, 500);
+window.addEventListener('load', () => {
+	// открытие шторки
+	setTimeout(() => {
+		document.body.classList.add('open');
+	}, 500);
+
+	// ===== персональные имена =====
+	const params = new URLSearchParams(window.location.search);
+	const guest = params.get('guest');
+
+	const el = document.getElementById('names');
+
+	const map = {
+		ruslan: 'Руслан & Елизавета',
+		anna: 'Анна & Иван',
+		parents: 'Дорогие родители',
+		friends: 'Дорогие друзья',
+	};
+
+	if (guest && map[guest] && el) {
+		el.textContent = map[guest];
+	}
 });
